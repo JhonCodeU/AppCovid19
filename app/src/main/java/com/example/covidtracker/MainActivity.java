03e7336bfbdb9ced433e2ba18344fe5eeb28046d
JhonCode <jhon.380172793@ucaldas.edu.co>
2021-06-03 20:50:30 -0500

    app finish

--- app/src/main/java/com/example/covidtracker/MainActivity.java
+++ app/src/main/java/com/example/covidtracker/MainActivity.java
@@ -3,12 +3,112 @@ package com.example.covidtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.covidtracker.api.ApiUtilities;
+import com.example.covidtracker.api.CountryData;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView totalConfirm, totalActive, totalRecovered, totalDeath, totalTests;
+    private TextView todayConfirm, todayRecovered, todayDeath, dateTv;
+    private PieChart pieChart;
+
+    private List<CountryData> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+
+        init();
+
+        ApiUtilities.getApiInterface().getCountryData()
+                .enqueue(new Callback<List<CountryData>>() {
+                    @Override
+                    public void onResponse(Call<List<CountryData>> call, Response<List<CountryData>> response) {
+                        list.addAll(response.body());
+
+                        for(int i = 0; i<list.size(); i++){
+                            if (list.get(i).getCountry().equals("Colombia")){
+                                int confirm = Integer.parseInt(list.get(i).getCases());
+                                int active = Integer.parseInt(list.get(i).getActive());
+                                int recovered = Integer.parseInt(list.get(i).getRecovered());
+                                int death = Integer.parseInt(list.get(i).getDeaths());
+                                int test = Integer.parseInt(list.get(i).getTests());
+
+                                totalActive.setText(NumberFormat.getInstance().format(active));
+                                totalConfirm.setText(NumberFormat.getInstance().format(confirm));
+                                totalRecovered.setText(NumberFormat.getInstance().format(recovered));
+                                totalDeath.setText(NumberFormat.getInstance().format(death));
+                                totalTests.setText(NumberFormat.getInstance().format(test));
+
+                                int tdConfirm = Integer.parseInt(list.get(i).getTodayCases());
+                                int tdRecovered = Integer.parseInt(list.get(i).getTodayRecovered());
+                                int tdDeath = Integer.parseInt(list.get(i).getTodayDeaths());
+
+                                todayConfirm.setText(NumberFormat.getInstance().format(tdConfirm));
+                                todayDeath.setText(NumberFormat.getInstance().format(tdRecovered));
+                                todayRecovered.setText(NumberFormat.getInstance().format(tdDeath));
+
+                                setText(list.get(i).getUpdated());
+
+                                pieChart.addPieSlice(new PieModel("Confirm", confirm, getResources().getColor(R.color.yellow)));
+                                pieChart.addPieSlice(new PieModel("Active", active, getResources().getColor(R.color.blue_pie)));
+                                pieChart.addPieSlice(new PieModel("Recovered", recovered, getResources().getColor(R.color.green)));
+                                pieChart.addPieSlice(new PieModel("Death", death, getResources().getColor(R.color.red_pie)));
+
+                                pieChart.startAnimation();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CountryData>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Error : "+t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void setText(String updated) {
+        DateFormat format = new SimpleDateFormat("MMM dd, yyyy");
+
+        long milliseconds = Long.parseLong(updated);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliseconds);
+
+        dateTv.setText("Updated at "+format.format(calendar.getTime()));
+    }
+
+    private void init(){
+
+        totalConfirm = findViewById(R.id.totalConfirm);
+        totalActive = findViewById(R.id.totalActive);
+        totalRecovered = findViewById(R.id.totalRecovered);
+        totalDeath = findViewById(R.id.totalDeath);
+        totalTests = findViewById(R.id.totalTest);
+        todayConfirm = findViewById(R.id.todayConfirm);
+        todayRecovered = findViewById(R.id.todayRecovered);
+        todayDeath = findViewById(R.id.todayDeath);
+        pieChart = findViewById(R.id.pieChart);
+        dateTv = findViewById(R.id.date);
     }
 }
